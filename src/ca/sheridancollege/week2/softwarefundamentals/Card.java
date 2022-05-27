fc91a0db5c34ab0dbd0b1b149a469e1ea0ff8589
marco chavez <chavezcm@sheridancollege.ca>
2022-05-27 15:19:14 -0400

    First commint Assign1

--- src/ca/sheridancollege/week2/softwarefundamentals/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/Card.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ca.sheridancollege.week2.softwarefundamentals;
 
 /**
@@ -21,6 +16,8 @@ public class Card
      * @param givenSuit
      * @param givenValue 
      */
+    // comments 3:18pm
+    // another comment
     public Card(String givenSuit, int givenValue)
     {
         suit = givenSuit;
